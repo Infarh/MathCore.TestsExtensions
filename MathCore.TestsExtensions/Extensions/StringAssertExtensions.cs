e83070fadf1b0508afcf2bf7dc672de9640c5448
Infarh <shmachilin@yandex.ru>
2019-12-19 15:00:14 +0300

    Комментарии...

--- MathCore.TestsExtensions/Extensions/StringAssertExtensions.cs
+++ MathCore.TestsExtensions/Extensions/StringAssertExtensions.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Microsoft.VisualStudio.TestTools.UnitTesting.Extensions
+namespace Microsoft.VisualStudio.TestTools.UnitTesting
 {
+    /// <summary>Методы-расширения для объекта проверки строк</summary>
     public static class StringAssertExtensions
     {
+        /// <summary>Проверка строки</summary>
+        /// <param name="that">Объект проверки</param>
+        /// <param name="ActualValue">Проверяемая строка</param>
+        /// <returns>Объект проверки строк</returns>
         public static ValueChecker<string> Value(StringAssert that, string ActualValue) => new ValueChecker<string>(ActualValue);
     }
 }
