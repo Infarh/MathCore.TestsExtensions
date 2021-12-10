85940ff2244b36ed35ea3ac4b8681783ad3d9ea6
Infarh <shmachilin@yandex.ru>
2021-12-10 09:58:11 +0300

    Исправление логики сравнения коллекции с перечислением

---
+++ Tests/MathCore.TestsExtensions.Tests/AssertTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MathCore.TestsExtensions.Tests
+{
+    public abstract class AssertTests
+    {
+        protected static AssertFailedException IsAssertFail(Action AssertAction) => ExpectedException<AssertFailedException>(AssertAction);
+
+        protected static TException ExpectedException<TException>(Action AssertAction) where TException : Exception
+        {
+            TException expected_exception = null;
+            try
+            {
+                AssertAction();
+            }
+            catch (TException exception)
+            {
+                expected_exception = exception;
+            }
+            if (expected_exception is null)
+                throw new AssertFailedException($"Требуемое исключение типа {typeof(TException).Name} выброшено не было");
+
+            return expected_exception;
+        }
+    }
+}
