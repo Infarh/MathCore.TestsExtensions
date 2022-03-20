5f4d2cb0dc1281f2bb01b4af5c6b2a528d5872af
Infarh <shmachilin@yandex.ru>
2022-03-20 23:27:29 +0300

    0.1.35.1 - Методы-расширения для упрощения синтаксиса тестирования

--- MathCore.TestsExtensions/Extensions/TestingExtensions.cs
+++ MathCore.TestsExtensions/Extensions/TestingExtensions.cs
@@ -14,4 +14,13 @@ public static class TestingExtensions
     public static EnumerableChecker<T> AssertThatEnumerable<T>(this IEnumerable<T> items) => Assert.That.Enumerable(items);
 
     public static EnumerableChecker<T> AssertEquals<T>(this IEnumerable<T> items, params T[] values) => Assert.That.Enumerable(items).IsEqualTo(values);
+
+    public static ValueChecker<double> AssertLessThan(this double ActualValue, double ExpectedValue) => Assert.That.Value(ActualValue).LessThan(ExpectedValue);
+    public static ValueChecker<double> AssertLessThan(this double ActualValue, double ExpectedValue, double Accuracy) => Assert.That.Value(ActualValue).LessThan(ExpectedValue, Accuracy);
+    public static ValueChecker<double> AssertLessOrEqualsThan(this double ActualValue, double ExpectedValue) => Assert.That.Value(ActualValue).LessOrEqualsThan(ExpectedValue);
+    public static ValueChecker<double> AssertLessOrEqualsThan(this double ActualValue, double ExpectedValue, double Accuracy) => Assert.That.Value(ActualValue).LessOrEqualsThan(ExpectedValue, Accuracy);
+    public static ValueChecker<double> AssertGreaterThan(this double ActualValue, double ExpectedValue) => Assert.That.Value(ActualValue).GreaterThan(ExpectedValue);
+    public static ValueChecker<double> AssertGreaterThan(this double ActualValue, double ExpectedValue, double Accuracy) => Assert.That.Value(ActualValue).GreaterThan(ExpectedValue, Accuracy);
+    public static ValueChecker<double> AssertGreaterOrEqualsThan(this double ActualValue, double ExpectedValue) => Assert.That.Value(ActualValue).GreaterOrEqualsThan(ExpectedValue);
+    public static ValueChecker<double> AssertGreaterOrEqualsThan(this double ActualValue, double ExpectedValue, double Accuracy) => Assert.That.Value(ActualValue).GreaterOrEqualsThan(ExpectedValue, Accuracy);
 }
