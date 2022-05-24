379627f8568e213a544e7f28b0ac9ef6594c2e9a
Infarh <shmachilin@yandex.ru>
2022-05-24 15:54:52 +0300

    0.1.39.1 - Добавлен механизм записи данных в журнал отладки теста в методе-обработчике результата

--- MathCore.TestsExtensions/Attributes/TestClassIterativeAttribute.cs
+++ MathCore.TestsExtensions/Attributes/TestClassIterativeAttribute.cs
@@ -3,8 +3,6 @@
 // ReSharper disable UnusedType.Global
 // ReSharper disable UnusedAutoPropertyAccessor.Global
 
-using System.Reflection;
-
 namespace Microsoft.VisualStudio.TestTools.UnitTesting;
 
 /// <summary>Итерационный тестовый класс</summary>
@@ -27,52 +25,5 @@ public class TestClassIterativeAttribute : TestClassAttribute
         var attribute = TestMethodAttribute as TestMethodIterativeAttribute ?? new TestMethodIterativeAttribute(_IterationsCount);
         attribute.StopAtFirstFail = StopAtFirstFail;
         return attribute;
-    }
-}
-
-[AttributeUsage(AttributeTargets.Method)]
-public class TestMethodHandlerAttribute : TestMethodAttribute
-{
-    public string? ExceptionHandlerMethod { get; set; }
-
-    public bool HandlePassed { get; set; }
-
-    public TestMethodHandlerAttribute() : this(null) { }
-
-    public TestMethodHandlerAttribute(string? ExceptionHandlerMethod, string? DisplayName = null) : base(DisplayName) =>
-        this.ExceptionHandlerMethod = ExceptionHandlerMethod;
-
-    public override TestResult[] Execute(ITestMethod Method)
-    {
-        if (ExceptionHandlerMethod is not { Length: > 0 } handler_method_name)
-            return base.Execute(Method);
-
-        var test_class = Method.MethodInfo.DeclaringType ?? throw new InvalidOperationException("Невозможно определить класс модульного теста");
-
-        //const BindingFlags public_instance = BindingFlags.Public | BindingFlags.Instance;
-        //const BindingFlags private_instance = BindingFlags.NonPublic | BindingFlags.Instance;
-        const BindingFlags public_static = BindingFlags.Public | BindingFlags.Static;
-        const BindingFlags private_static = BindingFlags.NonPublic | BindingFlags.Static;
-
-        var test_result_type = typeof(TestResult);
-        var handler_method_info =
-            //test_class.GetMethod(handler_method_name, public_instance, null, new[] { test_result_type }, null) ??
-            //test_class.GetMethod(handler_method_name, private_instance, null, new[] { test_result_type }, null) ??
-            test_class.GetMethod(handler_method_name, public_static, null, new[] { test_result_type }, null) ??
-            test_class.GetMethod(handler_method_name, private_static, null, new[] { test_result_type }, null);
-
-        if (handler_method_info is null)
-            return base.Execute(Method);
-
-        var result = base.Execute(Method);
-
-        IEnumerable<TestResult> results_to_process = HandlePassed
-            ? result
-            : result.Where(r => r.Outcome != UnitTestOutcome.Passed);
-
-        foreach (var test_result in results_to_process)
-            handler_method_info.Invoke(null, new object[] { test_result });
-
-        return result;
     }
 }
---
+++ Tests/MathCore.TestsExtensions.Tests/TestClassHandlerAttributeTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MathCore.TestsExtensions.Tests;
+
+[TestClassHandler("ResultHandler", HandlePassed = true)]
+public class TestClassHandlerAttributeTests
+{
+    [TestMethod]
+    public void ExecutionTest()
+    {
+        //Assert.Fail();
+    }
+
+    private static void ResultHandler(TestResult result)
+    {
+        Debug.WriteLine("Handler executed");
+        result.LogOutput += "123";
+    }
+}
